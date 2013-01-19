3fd1473417a2965ce44d696ba54cadcdd0de842c
Werner Lemberg <wl@gnu.org>
2013-01-19 18:15:14 +0100

    Run `update-copyright'.
    Minor other fixes.

--- doc/make-snapshot.sh
+++ doc/make-snapshot.sh
@@ -1,5 +1,16 @@
 #! /bin/sh
 #
+# Copyright (C) 2013 by Werner Lemberg.
+#
+# This file is part of the ttfautohint library, and may only be used,
+# modified, and distributed under the terms given in `COPYING'.  By
+# continuing to use, modify, or distribute this file you indicate that you
+# have read `COPYING' and understand and accept it fully.
+#
+# The file `COPYING' mentioned in the previous paragraph is distributed
+# with the ttfautohint library.
+#
+#
 # make-snapshot.sh <application> <filename>
 #
 # Make a snapshot from an application's start window and save it to a file.
