4597e97ed0af96dce2edbc91c5cde84b7bf91278
Werner Lemberg <wl@gnu.org>
2013-01-21 08:33:51 +0100

    Minor.

--- frontend/main.cpp
+++ frontend/main.cpp
@@ -293,7 +293,7 @@ show_version()
 #else
 "ttfautohint " VERSION "\n"
 #endif
-"Copyright (C) 2011-2012 Werner Lemberg <wl@gnu.org>.\n"
+"Copyright (C) 2011-2013 Werner Lemberg <wl@gnu.org>.\n"
 "License: FreeType License (FTL) or GNU GPLv2.\n"
 "This is free software: you are free to change and redistribute it.\n"
 "There is NO WARRANTY, to the extent permitted by law.\n");
--- frontend/maingui.cpp
+++ frontend/maingui.cpp
@@ -111,7 +111,7 @@ Main_GUI::about()
   QMessageBox::about(this,
                      tr("About TTFautohint"),
                      tr("<p>This is <b>TTFautohint</b> version %1<br>"
-                        " Copyright %2 2011-2012<br>"
+                        " Copyright %2 2011-2013<br>"
                         " by Werner Lemberg <tt>&lt;wl@gnu.org&gt;</tt></p>"
                         ""
                         "<p><b>TTFautohint</b> adds new auto-generated hints"
