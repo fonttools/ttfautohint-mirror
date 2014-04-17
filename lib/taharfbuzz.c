d2adf4f26ef94b2c7c985c9d8c0ec628945afc60
Werner Lemberg <wl@gnu.org>
2014-04-17 15:10:30 +0200

    Synchronize with FreeType.
    This corresponds to commits 181fd071ee58b4b59257a2a162ea0a928057aa8d,
    `[afblue.pl]: Minor improvements.', and
    8b1f2a6cda1b0780e60f521a96e1422e48fb89e3, `[autofit] Improve coverage
    handling.'.

--- lib/taharfbuzz.c
+++ lib/taharfbuzz.c
@@ -307,8 +307,15 @@ ta_get_coverage(TA_FaceGlobals globals,
    * possible solution might be to directly parse the GPOS table to find
    * out whether a glyph gets shifted vertically, but this is something I
    * would like to avoid if not really necessary.
+   *
+   * Note that we don't follow this logic for the default coverage. 
+   * Complex scripts like Devanagari have mandatory GPOS features to
+   * position many glyph elements, using mark-to-base or mark-to-ligature
+   * tables; the number of glyphs missed due to condition (b) would be far
+   * too large.
    */
-  hb_set_subtract(gsub_glyphs, gpos_glyphs);
+  if (style_class->coverage != TA_COVERAGE_DEFAULT)
+    hb_set_subtract(gsub_glyphs, gpos_glyphs);
 
 #ifdef TA_DEBUG
   TA_LOG_GLOBAL(("  glyphs without GPOS data (`*' means already assigned)"));
