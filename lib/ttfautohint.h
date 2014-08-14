db2c6c41a5863363e646345d9ac709914aed2740
Werner Lemberg <wl@gnu.org>
2014-08-14 15:37:09 +0200

    [doc] Use subscripts.

--- lib/ttfautohint.h
+++ lib/ttfautohint.h
@@ -359,8 +359,8 @@ typedef int
  *     also.
  *
  * `increase-x-height`
- * :   An integer.  For PPEM values in the range 6\ <= PPEM <=\
- *     `increase-x-height`, round up the font's x\ height much more often
+ * :   An integer.  For PPEM values in the range 6\ <= PPEM
+ *     <= `increase-x-height`, round up the font's x\ height much more often
  *     than normally (to use the terminology of TrueType's 'Super Round'
  *     bytecode instruction, the threshold gets increased from 5/8px to
  *     13/16px).  If it is set to\ 0, this feature is switched off.  If this
@@ -371,14 +371,14 @@ typedef int
  * :   A pointer of type `const char*` to a null-terminated string that
  *     gives a list of comma separated PPEM values or value ranges at which
  *     no x\ height snapping shall be applied.  A value range has the form
- *     *value1*`-`*value2*, meaning *value1* <= PPEM <= *value2*.  *value1*
- *     or *value2* (or both) can be missing; a missing value is replaced by
- *     the beginning or end of the whole interval of valid PPEM values,
- *     respectively.  Whitespace is not significant; superfluous commas are
- *     ignored, and ranges must be specified in increasing order.  For
- *     example, the string `"3, 5-7, 9-"` means the values 3, 5, 6, 7, 9,
- *     10, 11, 12, etc.  Consequently, if the supplied argument is `"-"`, no
- *     x\ height snapping takes place at all.  The default is the empty
+ *     *value*~1~`-`*value*~2~, meaning *value*~1~ <= PPEM <= *value*~2~.
+ *     *value*~1~ or *value*~2~ (or both) can be missing; a missing value is
+ *     replaced by the beginning or end of the whole interval of valid PPEM
+ *     values, respectively.  Whitespace is not significant; superfluous
+ *     commas are ignored, and ranges must be specified in increasing order.
+ *     For example, the string `"3, 5-7, 9-"` means the values 3, 5, 6, 7,
+ *     9, 10, 11, 12, etc.  Consequently, if the supplied argument is `"-"`,
+ *     no x\ height snapping takes place at all.  The default is the empty
  *     string (`""`), meaning no snapping exceptions.
  *
  * `windows-compatibility`
