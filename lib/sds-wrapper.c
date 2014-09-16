90ee43dcedaf746b83f84a0dd2d1b664d73af542
Werner Lemberg <wl@gnu.org>
2014-09-16 11:22:30 +0200

    Add `sds' string library.
    This lightweight dynamic string library was stripped off from the `redis'
    project by its main author; the original git repository can be found at
    
      https://github.com/antirez/sds    .
    
    I've cloned it, applying a bunch of pull requests for `antirez/sds', and
    also fixing various smaller issues, see
    
      https://github.com/lemzwerg/sds    .
    
    The imported files are taken from commit 6c1bd90e.

---
+++ lib/sds-wrapper.c
@@ -0,0 +1,25 @@
+/* sds-wrapper.c */
+
+/*
+ * Copyright (C) 2014 by Werner Lemberg.
+ *
+ * This file is part of the ttfautohint library, and may only be used,
+ * modified, and distributed under the terms given in `COPYING'.  By
+ * continuing to use, modify, or distribute this file you indicate that you
+ * have read `COPYING' and understand and accept it fully.
+ *
+ * The file `COPYING' mentioned in the previous paragraph is distributed
+ * with the ttfautohint library.
+ */
+
+
+/* Load `config.h' before library source code. */
+#include "config.h"
+
+/* for va_copy, via gnulib -- */
+/* we don't enforce a fully C99-compliant compiler */
+#include <stdarg.h>
+
+#include "sds.c"
+
+/* end of sds-wrapper.c */
