245df6faf1c1b2a393692d0a2e99f854e27fd5b7
Werner Lemberg <wl@gnu.org>
2014-09-16 23:45:55 +0200

    tadump.c: Use sds.

--- lib/tadump.c
+++ lib/tadump.c
@@ -17,157 +17,34 @@
 #include <stdarg.h>
 
 
-typedef struct elem_
-{
-  char* s;
-  int len;
-
-  struct elem_* next;
-} elem;
-
-
-
 #define DUMPVAL(str, arg) \
           do \
           { \
-            list = list_prepend(list, \
-                                "%*s = %ld\n", \
-                                width, (str), (FT_Long)(arg)); \
-            if (!list) \
-              goto Fail; \
+            s = sdscatprintf(s, \
+                             "%*s = %ld\n", \
+                             width, (str), (FT_Long)(arg)); \
+            if (!s) \
+              goto Exit; \
           } while (0)
 #define DUMPSTR(str, arg) \
           do \
           { \
-            list = list_prepend(list, \
-                                "%*s = %s%s", \
-                                width, (str), (arg), eol); \
-            if (!list) \
-              goto Fail; \
+            s = sdscatprintf(s, \
+                             "%*s = %s%s", \
+                             width, (str), (arg), eol); \
+            if (!s) \
+              goto Exit; \
           } while (0)
 #define DUMPSTRX(arg) \
           do \
           { \
-            list = list_prepend(list, \
-                                "%s%*s   %s%s", \
-                                prev_eol, width, "", (arg), eol); \
-            if (!list) \
-              goto Fail; \
+            s = sdscatprintf(s, \
+                             "%s%*s   %s%s", \
+                             prev_eol, width, "", (arg), eol); \
+            if (!s) \
+              goto Exit; \
           } while (0)
 
-
-void
-list_free(elem* list)
-{
-  while (list)
-  {
-    elem* tmp;
-
-
-    tmp = list;
-    list = list->next;
-    free(tmp->s);
-    free(tmp);
-  }
-}
-
-
-elem*
-list_prepend(elem* list,
-             const char* fmt,
-             ...)
-{
-  va_list ap;
-  char* s;
-  int len;
-
-  elem* e;
-
-
-  va_start(ap, fmt);
-  len = vasprintf(&s, fmt, ap);
-  va_end(ap);
-
-  if (len == -1)
-  {
-    list_free(list);
-    return NULL;
-  }
-
-  e = (elem*)malloc(sizeof (elem));
-  if (!e)
-  {
-    free(s);
-    list_free(list);
-    return NULL;
-  }
-
-  e->s = s;
-  e->len = len;
-  e->next = list;
-
-  return e;
-}
-
-
-elem*
-list_reverse(elem* list)
-{
-  elem* cur;
-
-
-  cur = list;
-  list = NULL;
-
-  while (cur)
-  {
-    elem* tmp;
-
-
-    tmp = cur;
-    cur = cur->next;
-    tmp->next = list;
-    list = tmp;
-  }
-
-  return list;
-}
-
-
-char*
-list_to_string(elem* list)
-{
-  elem* e;
-  int len;
-
-  char* buf;
-  char* bufp;
-
-
-  e = list;
-  len = 0;
-  while (e)
-  {
-    len += e->len;
-    e = e->next;
-  }
-
-  buf = (char*)malloc(len + 1);
-  if (!buf)
-    return NULL;
-  buf[0] = '\0';
-
-  e = list;
-  bufp = buf;
-  while (e)
-  {
-    strcpy(bufp, e->s);
-    bufp += e->len;
-    e = e->next;
-  }
-
-  return buf;
-}
 
 
 /* if `format' is set, we present the data in a more friendly format */
@@ -176,8 +53,9 @@ char*
 TA_font_dump_parameters(FONT* font,
                         FT_Bool format)
 {
-  char* buf;
-  elem* list = NULL;
+  sds s;
+  size_t len;
+  char* res;
 
   char* ns = NULL;
   char* ds = NULL;
@@ -187,13 +65,17 @@ TA_font_dump_parameters(FONT* font,
   const char* prev_eol = "";
 
 
+  s = sdsempty();
+  if (!s)
+    return NULL;
+
   if (format)
   {
-    list = list_prepend(list, "TTF_autohint parameters\n"
-                              "=======================\n"
-                              "\n");
-    if (!list)
-      goto Fail;
+    s = sdscat(s, "TTF_autohint parameters\n"
+                  "=======================\n"
+                  "\n");
+    if (!s)
+      goto Exit;
 
     width = 33;
   }
@@ -242,13 +124,21 @@ TA_font_dump_parameters(FONT* font,
   ns = number_set_show(font->x_height_snapping_exceptions,
                        TA_PROP_INCREASE_X_HEIGHT_MIN, 0x7FFF);
   if (!ns)
-    goto Fail;
+  {
+    sdsfree(s);
+    s = NULL;
+    goto Exit;
+  }
 
   DUMPSTR("x-height-snapping-exceptions", ns);
 
   ds = TA_deltas_show(font);
   if (!ds)
-    goto Fail;
+  {
+    sdsfree(s);
+    s = NULL;
+    goto Exit;
+  }
 
   /* show delta exceptions data line by line */
   if (!format)
@@ -280,25 +170,30 @@ TA_font_dump_parameters(FONT* font,
 
   if (!format)
   {
-    list = list_prepend(list, "\n");
-    if (!list)
-      goto Fail;
+    s = sdscat(s, "\n");
+    if (!s)
+      goto Exit;
   }
 
-  list = list_prepend(list, "\n");
-  if (!list)
-    goto Fail;
+  s = sdscat(s, "\n");
+  if (!s)
+    goto Exit;
 
 Exit:
-  list = list_reverse(list);
-  buf = list_to_string(list);
-
-Fail:
   free(ns);
   free(ds);
-  list_free(list);
 
-  return buf;
+  if (!s)
+    return NULL;
+
+  len = sdslen(s) + 1;
+  res = (char*)malloc(len);
+  if (res)
+    memcpy(res, s, len);
+
+  sdsfree(s);
+
+  return res;
 }
 
 /* end of tadump.c */
