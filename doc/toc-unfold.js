d4135ba9c8388b02d31397583782e621542567ab
Werner Lemberg <wl@gnu.org>
2014-12-22 20:40:41 +0100

    [doc] Implement folded TOC in HTML.

---
+++ doc/toc-unfold.js
@@ -0,0 +1,37 @@
+/*!
+ * toc-unfold.js
+ *
+ * JavaScript code to fold and unfold TOC entries
+ *
+ * written 2014 by Werner Lemberg
+ */
+
+// This code snippet needs `jquery' (http://code.jquery.com/jquery-1.11.2.js)
+
+$(document).ready(function() {
+  TOC.setup();
+});
+
+var TOC = {
+  setup: function() {
+    /* decorate TOC's `li' elements that have children with `folded' class */
+    /* (pandoc doesn't do this) */
+    $("#TOC li").each(function(i) {
+      if ($(this).children().length) {
+        $(this).addClass("folded");
+      }
+    });
+
+    /* fold all entries */
+    $(".folded").each(function(i) {
+      $(this).children("ul:first").hide();
+    });
+
+    /* change class to `unfolded' if clicked */
+    $(".folded").click(function(e) {
+      $(this).toggleClass("unfolded");
+      $(this).children("ul:first").slideToggle("300");
+      e.stopPropagation();
+    });  
+  }
+}
