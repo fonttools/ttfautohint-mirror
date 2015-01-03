05a9fbdde48d209b12d7cead966fb5020c7d31d4
Werner Lemberg <wl@gnu.org>
2015-01-03 09:30:50 +0100

    Update copyright year.

--- frontend/ttlineedit.h
+++ frontend/ttlineedit.h
@@ -1,6 +1,6 @@
 // ttlineedit.h
 
-// Copyright (C) 2012-2014 by Werner Lemberg.
+// Copyright (C) 2012-2015 by Werner Lemberg.
 //
 // This file is part of the ttfautohint library, and may only be used,
 // modified, and distributed under the terms given in `COPYING'.  By
