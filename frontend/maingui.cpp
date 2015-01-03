70393bba8940c76f600926a52911fd693316fded
Werner Lemberg <wl@gnu.org>
2015-01-03 09:40:54 +0100

    Minor.

--- frontend/maingui.cpp
+++ frontend/maingui.cpp
@@ -211,7 +211,7 @@ Main_GUI::about()
   QMessageBox::about(this,
                      tr("About TTFautohint"),
                      tr("<p>This is <b>TTFautohint</b> version %1<br>"
-                        " Copyright %2 2011-2014<br>"
+                        " Copyright %2 2011-2015<br>"
                         " by Werner Lemberg <tt>&lt;wl@gnu.org&gt;</tt></p>"
                         ""
                         "<p><b>TTFautohint</b> adds new auto-generated hints"
