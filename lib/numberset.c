df6454c8a4aebd31b38dc155b6bd6c9538e60689
Werner Lemberg <wl@gnu.org>
2019-01-25 12:09:44 +0100

    Update copyright year.

--- lib/numberset.c
+++ lib/numberset.c
@@ -1,7 +1,7 @@
 /* numberset.c */
 
 /*
- * Copyright (C) 2012-2018 by Werner Lemberg.
+ * Copyright (C) 2012-2019 by Werner Lemberg.
  *
  * This file is part of the ttfautohint library, and may only be used,
  * modified, and distributed under the terms given in `COPYING'.  By
